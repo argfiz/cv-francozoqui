37eb0547ae4a835770b3ad97f09529a77ffd3dbf
argfiz <francozoqui@gmail.com>
2024-05-30 18:58:20 -0300

    Subiendo archivos restantes

---
+++ data.js
@@ -0,0 +1,74 @@
+const proyectos = [
+    {
+      titulo: "CRM personalizado",
+      descripcion:
+        "CRM creado desde cero para administrar la relación con los clientes de una empresa de seguros. Incluye la gestión de leads desde distintos roles, parametrización viva, roles y permisos. El proyecto está actualmente en curso.",
+      link: "",
+      fecha: "01/12/2023",
+      imagen: "NDA.png",
+      tecnologias: ["Angular",""],
+    },
+    {
+      titulo: "Curso Front end con Angular",
+      descripcion:
+        "Curso que se compone de 8 horas de puro contenido separado en 17 videos en donde enseño como crear aplicaciones web utilizando Angular.",
+      link: "https://www.youtube.com/playlist?list=PL3Qv7aeTNq0eChWdLTbxelz-5d6ZTBp3i",
+      fecha: "01/06/2023",
+      imagen: "curso_angular_1.png",
+      tecnologias: ["HTML","CSS","Javascript"],
+    },
+    {
+      titulo: "Conector Facebook Ads → Engage CRM",
+      descripcion:
+        "Servicio web que permite sincronizar los leads de una campaña de marketing de Facebook Ads con un CRM llamado Engage.",
+      link: "",
+      fecha: "01/08/2023",
+      imagen: "NDA.png",
+      tecnologias: ["HTML","CSS","Javascript"],
+    },
+    {
+        titulo: "Conector Facebook Ads → Engage CRM",
+        descripcion:
+          "Servicio web que permite sincronizar los leads de una campaña de marketing de Facebook Ads con un CRM llamado Engage.",
+        link: "",
+        fecha: "01/08/2023",
+        imagen: "NDA.png",
+        tecnologias: ["HTML","CSS","Javascript"],
+    },
+    {
+        titulo: "Conector Facebook Ads → Engage CRM",
+        descripcion:
+          "Servicio web que permite sincronizar los leads de una campaña de marketing de Facebook Ads con un CRM llamado Engage.",
+        link: "",
+        fecha: "01/08/2023",
+        imagen: "NDA.png",
+        tecnologias: ["HTML","CSS","Javascript"],
+    },  
+  ];
+  
+  const informacionPersonal = {
+    nombre: "Franco Ignacio Zoqui",
+    subtitulo: "Desarrollador web",
+    imagen: "img/calo1.png",
+    otros: [
+      ["Nacionalidad", "Argentino"],
+      [
+        "Edad",
+        new Date(new Date() - new Date("1990/01/18")).getFullYear()-1970 + " años",
+      ],
+    ],
+    idiomas: [
+      ["Español", "Nativo"],["Inglés", "Bueno"],
+    ],
+    tecnologias: [
+      ["Html", 9],["Css", 8],["Javascript", 9],["Typescript", 8],["Angular", 8
+      ],["Ionic",7],["Node",6],["Python",5],["C#",3]
+    ],
+    redes: [
+      ["github", "https://github.com/cacalo"],
+      ["linkedin", "https://www.linkedin.com/in/gonzalo-bechara-baladi-05b14353/"],
+      ["youtube", "https://www.youtube.com/@puntoJson"],
+      ["instagram", ""],
+    ]
+  };
+  
